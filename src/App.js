0268ad3b834d7eeb15500673dd7b40bae71ee6c3
Quentin Marque <quentinm2000@hotmail.fr>
2022-02-21 22:53:42 +0100

    feat: 🎸 Added genre component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 import About from "./About";
 import Home from "./Home";
-import routes from "./routes";
-import User from "./User";
-import Users from "./Users";
 
 function App() {
   return (
@@ -13,15 +10,12 @@ function App() {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
-          <Link to="/users">Users</Link>
         </nav>
       </header>
       <main>
         <Routes>
-          <Route path={routes.home} element={<Home />} />
+          <Route path="/users/:id" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/users/:id" element={<User />} />
         </Routes>
       </main>
     </BrowserRouter>
