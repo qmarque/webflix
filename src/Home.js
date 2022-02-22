3cd17208bd3b4604394a4c74ec7bc84d7bd341cc
Quentin Marque <quentinm2000@hotmail.fr>
2022-02-22 22:30:51 +0100

    feat: 🎸 Synchronization URL and search input

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import Input from "./Input";
 import VerticalList from "./VerticalList";
 
@@ -7,11 +8,16 @@ import useStyles from "./Home.style";
 
 function Home() {
     const classes = useStyles();
-    const [value, setValue] = useState("");
-    const onChange = (event) => setValue(event.target.value);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [value, setValue] = useState(searchParams.get("q"));
+    const onChange = (event) => {
+      setValue(event.target.value);
+      setSearchParams(event.target.value ? { q: event.target.value } : {});
+    };
     const movies = data.movies.filter((movie) =>
     movie.title.match(new RegExp(value, "i"))
-  );    return (
+    );    
+    return (
         <div className={classes.root}>
         <Input value={value} onChange={onChange} />
         <VerticalList className={classes.list} data={movies} />
